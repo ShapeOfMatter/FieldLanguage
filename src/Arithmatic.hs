80ba1190b769867c303f0a2c12e5a400b1d72e7a
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2020-10-14 21:24:08 -0400

    Professionality to the wind; it is absolute bullshit that a haskell project runs at all this early in the process.

--- src/Arithmatic.hs
+++ src/Arithmatic.hs
@@ -3,7 +3,7 @@ module Arithmatic where
 -- Stolen from https://rosettacode.org/wiki/Modular_inverse#Haskell  (Gotta worry about licence compatability!)
 -- Given a and m, return Just x such that ax = 1 mod m.
 -- If there is no such x return Nothing.
-modInv :: Integeral a => a -> a -> Maybe a
+modInv :: Integral a => a -> a -> Maybe a
 modInv a m
   | 1 == g = Just (mkPos i)
   | otherwise = Nothing
@@ -17,7 +17,7 @@ modInv a m
 -- Given non-negative a and b, return x, y and g
 -- such that ax + by = g, where g = gcd(a,b).
 -- Note that x or y may be negative.
-gcdExt :: Integeral a => a -> a -> (a, a, a)
+gcdExt :: Integral a => a -> a -> (a, a, a)
 gcdExt a 0 = (1, 0, a)
 gcdExt a b =
   let (q, r) = a `quotRem` b
--- src/Main.hs
+++ src/Main.hs
@@ -35,31 +35,32 @@ data Expression = BaseValue Value
 
 type TypeContext = [Variable]
 lookupType :: TypeContext -> String -> Type
-lookupType v:gamma n = if n == (nameOfVar v) then typeOfVar v else lookupType gamma n
+lookupType (v:gamma) n = if n == (nameOfVar v) then typeOfVar v else lookupType gamma n
 lookupType [] _ = undefined  -- Gotta represent failure somehow!
 
 typeOf :: TypeContext -> Expression -> Type
-typeOf gamma = tInG
-    where tInG (BaseValue (Element prime _)) = Field prime,
-          tInG (BaseValue (Lambda var body)) = Function (typeOfVar var) $ typeOf (var:gamma) body
-          tInG (BaseVariable name) = gamma `lookupType` name
-          tInG (Negative exp) = tInG exp
-          tInG (Inverse exp) = tInG exp
-          tInG (Addition expL expR) = let t = tIng expL
-                                      in if t == (tInG expR) then t else undefined
-          tIng (Multiplication expL expr) = let t = tIng expL
-                                            in if t == (tInG expR) then t else undefined
-          tInG (Application func val) = let tval = tInG val
-                                            (from, to) = lambdaType $ tIng func
-                                        in if tval == from then to else undefined  -- TODO: Better error reporting
-          tInG (Let var val body) = tInG $ Application (Lambda var body) val
-          tInG (Conditional pred a b) = let ta = tInG a
-                                        in if (tInG pred) == Field 2 && ta == (tInG b) then ta else undefined
+typeOf gamma e = case e of
+    BaseValue (Element prime _) -> Field prime
+    BaseValue (Lambda var body) -> Function (typeOfVar var) $ typeOf (var:gamma) body
+    BaseVariable name -> gamma `lookupType` name
+    Negative exp -> tInG exp
+    Inverse exp -> tInG exp
+    Addition expL expR -> let t = tInG expL
+                          in if t == (tInG expR) then t else undefined
+    Multiplication expL expR -> let t = tInG expL
+                                in if t == (tInG expR) then t else undefined
+    Application func val -> let tval = tInG val
+                                (from, to) = lambdaType $ tInG func
+                            in if tval == from then to else undefined  -- TODO: Better error reporting
+    Let var val body -> tInG $ Application (BaseValue (Lambda var body)) val
+    Conditional pred a b -> let ta = tInG a
+                            in if (tInG pred) == Field 2 && ta == (tInG b) then ta else undefined
+    where tInG = typeOf gamma
 
 type EvalContext = [(Variable, Expression)]
 lookupVariable :: EvalContext -> String -> Expression -- If we could make this stateful it'd be a huge boost in performance.
-lookupVariable (var, val):gamma n = if n == (nameOfVar var) then val else lookupVariable gamma n
-lookupVariable [] _ undefined
+lookupVariable ((var, val):gamma) n = if n == (nameOfVar var) then val else lookupVariable gamma n
+lookupVariable [] _ = undefined
 
 evalExp :: EvalContext -> Expression -> Expression
 evalExp gamma = eInG
@@ -72,7 +73,7 @@ evalExp gamma = eInG
           -- Inverse cases
           eInG (Inverse (BaseValue (Element prime val))) = BaseValue $ maybe undefined (Element prime) (val `modInv` prime)
           eInG (Inverse (BaseValue _)) = undefined
-          eInG (Inverse exp) = Inverse eInG exp
+          eInG (Inverse exp) = Inverse $ eInG exp
           -- Addition cases
           eInG (Addition (BaseValue (Element prime val1)) (BaseValue (Element _ val2))) = BaseValue $ Element prime ((val1 + val2) `mod` prime)
           eInG (Addition exp1@(BaseValue _) exp2) = Addition exp1 (eInG exp2)
@@ -82,19 +83,19 @@ evalExp gamma = eInG
           eInG (Multiplication exp1@(BaseValue _) exp2) = Multiplication exp1 (eInG exp2)
           eInG (Multiplication exp1 exp2@(BaseValue _)) = Multiplication (eInG exp1) exp2  -- No explicit failure for Lambda!?
           -- Application cases
-          eInG (Application (BaseValue (Lambda var body)) arg) = evalExp (var, arg):gamma body
-          eInG (Application (BaseValue _) _ = undefined
+          eInG (Application (BaseValue (Lambda var body)) arg) = evalExp ((var, arg):gamma) body
+          eInG (Application (BaseValue _) _) = undefined
           eInG (Application funcExp arg) = Application (eInG funcExp) arg
           -- Let cases (Let is just sugar!)
           eInG (Let var val body) = Application (BaseValue (Lambda var body)) val
           -- Conditional cases
-          eInG (Conditional (BaseValue (Element 2 b) expT expF) = eInG $ if 1 == b then expT else expF
+          eInG (Conditional (BaseValue (Element 2 b)) expT expF) = eInG $ if 1 == b then expT else expF
           eInG (Conditional (BaseValue _) _ _) = undefined
-          eInG (Conditional predicate expT expF = Conditional (eInG predicate) expT expF
+          eInG (Conditional predicate expT expF) = Conditional (eInG predicate) expT expF
 
 evaluateWith :: EvalContext -> Expression -> Value
 evaluateWith _ (BaseValue val) = val
-evaluateWith gamma exp = evaluateExpression $ evalExp gamma exp
+evaluateWith gamma exp = evaluateWith gamma $ evalExp gamma exp
 evaluate = evaluateWith []
 
 
